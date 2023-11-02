db95f44f400778829966b54d400cbd268633c042
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-11-02 15:37:17 -0300

    Update Funcionario.cs

--- Atores/Funcionario.cs
+++ Atores/Funcionario.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Atores
+namespace GerenciamentoEstoque.Atores.Internos
 {
     public class Funcionario
     {
         // Atributos
-        protected int id;
+        protected string id;
         protected string nome;
         protected string cpf;
         protected string email;
@@ -17,22 +17,13 @@ namespace ConsoleApp1.Atores
         protected string senha;
         protected string endereco;
         protected string telefone;
+        protected char tipoFuncionario;
+        private bool estaLogado = false;
 
-        // Cadastro sem telefone
-        public Funcionario(string nome, string cpf, string email, string login, string senha, string endereco)
+        // Cadastro
+        public Funcionario(string nome, string cpf, string email, string login, string senha, string endereco, string telefone = "")
         {
-            // Gerar id aleatório entre 000001 e 999999
-            this.nome = nome;
-            this.cpf = cpf;
-            this.email = email;
-            this.login = login;
-            this.senha = senha;
-            this.endereco = endereco;
-        }
-        // Cadastro com telefone
-        public Funcionario(string nome, string cpf, string email, string login, string senha, string endereco, string telefone)
-        {
-            // Gerar id aleatório entre 000001 e 999999
+            SetId();
             this.nome = nome;
             this.cpf = cpf;
             this.email = email;
@@ -43,28 +34,76 @@ namespace ConsoleApp1.Atores
         }
 
         // Métodos
-        public void Login(string login, string senha)
+        public bool Login()
         {
+            while (true)
+            {
+                Console.Write("Digite seu login: ");
+                string login = Console.ReadLine();
+                Console.Write("Digite sua senha: ");
+                string senha = Console.ReadLine();
 
+                if (this.login == login && this.senha == senha)
+                {
+                    Console.WriteLine("Login bem-sucedido!");
+                    estaLogado = true;
+                    return true;
+                }
+
+                Console.WriteLine("Login falhou. Verifique suas credenciais.");
+            }
         }
+
         public void Logoff()
         {
-
+            Console.WriteLine("Você saiu. Até mais! :)");
+            estaLogado = false;
         }
-        public void AlterarSenha(string novaSenha)
-        {
 
+        public void AlterarSenha()
+        {
+            Console.Write("\nDigite sua senha atual: ");
+            string senhaAntiga = Console.ReadLine();
+            while (senhaAntiga != senha)
+            {
+                Console.WriteLine("\nSenha incorreta! Tente novamente.");
+                Console.Write("\nDigite sua senha atual: ");
+                senhaAntiga = Console.ReadLine();
+            }
+
+            Console.Write("\nDigite sua nova senha: ");
+            string senhaNova = Console.ReadLine();
+            Console.Write("\nRepita sua nova senha: ");
+            string senhaConfirmacao = Console.ReadLine();
+
+            while (senhaNova != senhaConfirmacao)
+            {
+                Console.WriteLine("\nAs senhas não coincidem! Tente novamente.");
+                Console.Write("\nDigite sua nova senha: ");
+                senhaNova = Console.ReadLine();
+                Console.Write("\nRepita sua nova senha: ");
+                senhaConfirmacao = Console.ReadLine();
+            }
+
+            senha = senhaNova;
+            Console.WriteLine("\nSenha alterada com sucesso!");
         }
 
         // Gets
-        public int GetId() { return id; }
+        public string GetId() { return id; }
         public string GetNome() { return nome; }
         public string GetCpf() { return cpf; }
         public string GetEmail() { return email; }
         public string GetEndereco() { return endereco; }
         public string GetTelefone() { return telefone; }
 
-        // Set
+        // Sets
+        private void SetId()
+        {
+            Random random = new Random();
+            int numeroAleatorio = random.Next(1, 1000000);
+            id = numeroAleatorio.ToString("000000");
+        }
         public void SetNome(string nome) { this.nome = nome; }
         public void SetEmail(string email) { this.email = email; }
         public void SetEndereco(string endereco) { this.endereco = endereco; }
