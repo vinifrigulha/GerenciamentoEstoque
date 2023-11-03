ad13da3ddc214d52bd7695e35c6826fdd05b9c80
PauloKuster <103347114+Radicaenta@users.noreply.github.com>
2023-11-03 20:08:07 -0300

    Update Fornecedor.cs
    Adicionado código "bruto" do fornecedor

--- Atores/Fornecedor.cs
+++ Atores/Fornecedor.cs
@@ -1,12 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Atores
+namespace Gerenciamentodeestoque.Atores
 {
-    internal class Fornecedor
+    public class Fornecedor
     {
+        private string idFornecedor;
+        private string nomeFornecedor;
+
+        public string GetId() { return idFornecedor; }
+        public string GetNome() { return nomeFornecedor; }
+
+        private void SetIdF()
+        {
+            Random random = new Random();
+            int numeroAleatorio = random.Next(1, 1000000);
+            idFornecedor = numeroAleatorio.ToString("000000");
+        }
+
+        public void SetNomeF()
+        {
+            Random random = new Random();
+            int numeroAleatorio = random.Next(1, 1000000);
+            idFornecedor = numeroAleatorio.ToString("000000");
+        }
+
+        public Cotacao ResponderCotacao(string idCotacao)
+        {
+
+        }
+
+        public void EnviarProdutos()
+        {
+
+        }
+
+        public void ReceberNotificaçãoAlmoxarife()
+        {
+
+        }
     }
 }
