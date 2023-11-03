450694bfb21ecaaa8a6eb09c696e39c2fce74d25
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2023-11-03 17:51:21 -0300

    Update banco_gerenciaEstoque.sql

--- Database/banco_gerenciaEstoque.sql
+++ Database/banco_gerenciaEstoque.sql
@@ -3,7 +3,7 @@ create database gerenciaEstoque;
 use gerenciaEstoque;
 
 create table funcionario(
-    idFuncionario integer,
+    idFuncionario integer not null,
     nome varchar(45),
     cpf char(11),
     email varchar(45),
@@ -15,14 +15,14 @@ create table funcionario(
 );
 
 create table requisicao(
-    idRequisicao integer,
+    idRequisicao integer not null,
     idProduto varchar(45),
     checado bool,
     primary key(idRequisicao,idProduto)
 );
 
 create table produto(
-    idProduto integer,
+    idProduto integer not null,
     preco double,
     qtdEstoque integer,
     localizacaoEstoque integer,
@@ -32,47 +32,106 @@ create table produto(
 );
 
 create table cotacao(
-    idCotacao integer,
-    idComprador integer,
+    idCotacao integer not null,
+    idComprador integer not null,
     finalizado bool,
     primary key(idCotacao, idComprador)
 );
 
 create table ordemCompra(
-    idOrdemCompra integer primary key
+    idOrdemCompra integer primary key not null
 );
 
 create table pedido(
-    idPedido integer,
-    idCliente integer,
+    idPedido integer not null,
+    idCliente integer not null,
     finalizado bool,
     primary key(idPedido, idCliente)
 );
 
 create table ordemVenda(
-    idOrdemVenda integer,
-    idVendedor integer,
+    idOrdemVenda integer not null,
+    idVendedor integer not null,
     primary key(idOrdemVenda, idVendedor)
 );
 
 create table produto(
-    idProduto integer ,
+    idProduto integer not null,
     preco double,
     quantidadeEstoque integer,
     localizacaoEstoque integer,
-    unidadeVenda string,
+    unidadeVenda varchar(45),
     validade datetime,
     primary key(idProduto)
 );
 
 create table fornecedor(
-    idFornecedor integer,
-    nomeFornecedor string,
+    idFornecedor integer not null,
+    nomeFornecedor varchar(45),
     primary key(idFornecedor)
 );
 
 create table cliente(
-    idCliente integer,
-    nome string,
+    idCliente integer not null,
+    nome varchar(45),
     primary key(idCliente)
 );
+
+CREATE TABLE ProdutoReq (
+    idRequisicao integer not null,
+    idFuncionario integer not null,
+    idCotacao integer not null,
+    idProduto integer not null,
+    foreign key (idRequisicao) references requisicao(idRequisicao),
+    foreign key (idFuncionario) references funcionario(idFuncionario),
+    foreign key (idCotacao) references cotacao(idCotacao),
+    foreign key (idProduto) references produto(idProduto)
+);
+
+alter table requisicao
+add constraint idFuncionario
+foreign key (idFuncionario)
+references funcionario(idFuncionario);
+
+alter table requisicao
+add constraint idCotacao
+foreign key (idCotacao)
+references cotacao(idCotacao);
+
+alter table produto
+add constraint idFuncionario
+foreign key (idFuncionario)
+references funcionario(idFuncionario);
+
+alter table pedido
+add constraint idFuncionario
+foreign key (idFuncionario)
+references funcionario(idFuncionario);
+
+alter table pedido
+add constraint idOrdemVenda
+foreign key (idOrdemVenda)
+references ordemVenda(idOrdemVenda);
+
+alter table cliente
+add constraint idFuncionario
+foreign key (idFuncionario)
+references funcionario(idFuncionario);
+
+alter table cliente
+add constraint idPedido
+foreign key (idPedido)
+references pedido(idPedido);
+
+alter table cliente
+add constraint idOrdemVenda
+foreign key (idOrdemVenda)
+references ordemVenda(idOrdemVenda);
+
+/*
+ALTER TABLE tabela_filha
+ADD CONSTRAINT nome_da_chave_estrangeira
+FOREIGN KEY (coluna_referencia)
+REFERENCES tabela_pai(coluna_referenciada);
+
+*/
