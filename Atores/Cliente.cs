7b20ce5635890f037d2f34189078c57c600ae2aa
PauloKuster <103347114+Radicaenta@users.noreply.github.com>
2023-11-03 20:32:28 -0300

    Update Cliente.cs
    Adicionado código bruto do cliente.

--- Atores/Cliente.cs
+++ Atores/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Gerenciamentodeestoque.Atores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,36 @@ namespace ConsoleApp1.Atores
 {
     internal class Cliente
     {
+        private string idCliente;
+        private string nomeCliente;
+
+        public string GetId() { return idCliente; }
+        public string GetNome() { return nomeCliente; }
+
+        private void SetId()
+        {
+            Random random = new Random();
+            int numeroAleatorio = random.Next(1, 1000000);
+            idCliente = numeroAleatorio.ToString("000000");
+        }
+        public void SetNome(string nomeCliente) { this.nomeCliente = nomeCliente; }
+
+        public void ResponderVendedor(string idVendedor) { 
+        }
+
+        public void Criarpedido(){
+
+        }
+
+        public void CancelarPedido(string idPedido)
+        {
+
+        }
+
+        public Pedido EnviarPedido(string idPedido){ 
+        }
+
+
+
     }
 }
