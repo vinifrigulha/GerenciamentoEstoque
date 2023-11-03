2f3c41407ae63197427d36bf87ee0578d077220c
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-11-03 20:50:44 -0300

    Update Comprador.cs

--- Atores/Comprador.cs
+++ Atores/Comprador.cs
@@ -1,12 +1,34 @@
-﻿using System;
+using GerenciamentoEstoque.Objetos.Estoque;
 using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Atores
+namespace GerenciamentoEstoque.Atores.Internos
 {
-    internal class Comprador
+    public class Comprador : Funcionario
     {
+        Dictionary<string, Cotacao> listaCotacao = new Dictionary<string, Cotacao>();
+        public Comprador(string nome, string cpf, string email, string login, string senha, string endereco, string telefone = "") 
+            : base(nome, cpf, email, login, senha, endereco, telefone)
+        {
+            this.tipoFuncionario = 'C';
+        }
+
+        // Cotações
+        public void CriarCotacao()
+        {
+            Cotacao cotacao = new Cotacao(id);
+            listaCotacao.Add(cotacao.GetIdCotacao(), cotacao);
+        }
+        public void ExibirCotacoes()
+        {
+            foreach (KeyValuePair<string, Cotacao> cotacao in listaCotacao)
+            {
+                Console.Write($"\nCotação {cotacao.Key} \nVendedor {cotacao.Value.GetIdComprador()} \nStatus {cotacao.Value.GetStatus()}");
+            }
+        }
+        
     }
 }
