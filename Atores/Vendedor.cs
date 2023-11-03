2fa30b0b8eb096c9880bc0efd86c4b0cc9c73440
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-11-03 20:51:00 -0300

    Update Vendedor.cs

--- Atores/Vendedor.cs
+++ Atores/Vendedor.cs
@@ -1,12 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Atores
+namespace GerenciamentoEstoque.Atores.Internos
 {
-    internal class Vendedor
+    public class Vendedor : Funcionario
     {
+        public Vendedor(string nome, string cpf, string email, string login, string senha, string endereco, string telefone = "") 
+            : base(nome, cpf, email, login, senha, endereco, telefone)
+        {
+            this.tipoFuncionario = 'V';
+        }
     }
 }
