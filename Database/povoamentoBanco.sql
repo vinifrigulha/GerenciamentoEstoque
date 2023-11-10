87f1c9b67e54663a729bbadf5f3b96bcf2848e88
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2023-11-10 14:14:43 -0300

    Update povoamentoBanco.sql

--- Database/povoamentoBanco.sql
+++ Database/povoamentoBanco.sql
@@ -1,17 +1,16 @@
--- INSERÇÔES PARA POVOAR O BANCO
-
 INSERT INTO funcionario (nome, cpf, email, senha, telefone, endereco, tipoFuncionario)
 VALUES
-    ('João Silva', '12345678901', 'joao@example.com', 'senha123', '(11) 1234-5678', 'Rua A, 123', 'A'),
-    ('Maria Santos', '23456789012', 'maria@example.com', 'senha456', '(12) 2345-6789', 'Rua B, 456', 'B'),
+    ('João Silva', '12345678901', 'joao@example.com', 'senha123', '(11) 1234-5678', 'Rua A, 123', 'C'),
+    ('Maria Santos', '23456789012', 'maria@example.com', 'senha456', '(12) 2345-6789', 'Rua B, 456', 'V'),
     ('Pedro Oliveira', '34567890123', 'pedro@example.com', 'senha789', '(13) 3456-7890', 'Rua C, 789', 'C'),
-    ('Ana Souza', '45678901234', 'ana@example.com', 'senhaABC', '(14) 4567-8901', 'Rua D, 901', 'D'),
-    ('Lucas Pereira', '56789012345', 'lucas@example.com', 'senhaDEF', '(15) 5678-9012', 'Rua E, 234', 'E'),
-    ('Mariana Ferreira', '67890123456', 'mariana@example.com', 'senhaGHI', '(16) 6789-0123', 'Rua F, 567', 'F'),
-    ('Gustavo Rodrigues', '78901234567', 'gustavo@example.com', 'senhaJKL', '(17) 7890-1234', 'Rua G, 678', 'G'),
-    ('Camila Lima', '89012345678', 'camila@example.com', 'senhaMNO', '(18) 8901-2345', 'Rua H, 789', 'H'),
-    ('Rafaela Alves', '90123456789', 'rafaela@example.com', 'senhaPQR', '(19) 9012-3456', 'Rua I, 890', 'I'),
-    ('Bruno Gomes', '01234567890', 'bruno@example.com', 'senhaSTU', '(20) 0123-4567', 'Rua J, 901', 'J');
+    ('Ana Souza', '45678901234', 'ana@example.com', 'senhaABC', '(14) 4567-8901', 'Rua D, 901', 'V'),
+    ('Breno Cardoso', '12345607222', 'benin@example.com', 'senhaLOL', '(27) 9983-4567', 'Rua B, 022', 'C'),
+    ('Gabriel Freire', '37845907689', 'biel@example.com', 'senhaBIBA', '(20) 3432-4567', 'Rua G, 456', 'V'),
+    ('Davi Ferreira', '39076845906', 'davizin@example.com', 'senhaANA', '(20) 4359-4567', 'Rua D, 232', 'C'),
+    ('Flavio Ortega', '18593038901', 'flavio@example.com', 'senhaFLA', '(20) 2321-4531', 'Rua F, 904', 'V'),
+    ('Cesar Augusto', '47890785688', 'cesaraugusto@example.com', 'senhaCESAR', '(20) 3241-4524', 'Rua CA, 333', 'V'),
+    ('Adriano Imperador', '85903286799', 'adriimpe@example.com', 'senhaIMP', '(20) 0123-4242', 'Rua AI, 171', 'V');
+
 
 INSERT INTO produto (preco, descricao, qtdEstoque, localizacaoEstoque, unidadeVenda, validade)
 VALUES
@@ -27,8 +26,6 @@ VALUES
     (14.99, 'Produto 10', 80, 'Prateleira J', 'Unidade', '2023-11-05');
     
 -- Os inserts na tabela requisicao dependem da relação com a tabela produto.
--- Suponhamos que você queira criar requisições para alguns produtos.
--- Vou inserir 10 requisições para os produtos de id 1 a 10.
 INSERT INTO requisicao (checado)
 VALUES
     (1),
@@ -55,9 +52,6 @@ VALUES
     (9, 1),
     (10, 0);
 
--- Inserir ordens de compra não faz sentido sem uma relação específica com outras tabelas.
--- Portanto, as ordens de compra são criadas quando há uma necessidade real no sistema.
--- Não é possível criar inserts genéricos para essa tabela sem mais contexto.
 
 INSERT INTO cliente (nome)
 VALUES
@@ -73,8 +67,6 @@ VALUES
     ('Rafaela Gonçalves');
 
 -- Para inserir dados na tabela pedido, você precisará associar os pedidos aos clientes e produtos.
--- Suponhamos que você queira criar pedidos para os produtos de id 1 a 10, associados aos clientes de id 1 a 10.
--- Vou inserir 10 pedidos, um para cada cliente e produto.
 
 INSERT INTO pedido (idCliente, finalizado)
 VALUES
@@ -103,8 +95,6 @@ VALUES
     ('Comércio de Produtos Alimentícios ABC');
 
 -- Para a tabela ProdutoReq, você precisará associar as requisições, funcionários e cotações com produtos específicos.
--- Suponhamos que você queira criar associações para os produtos de id 1 a 10, com funcionários de id 1 a 10 e cotações de id 1 a 10.
--- Vou inserir 10 associações.
 INSERT INTO ProdutoReq (idRequisicao, idFuncionario, idCotacao)
 VALUES
     (1, 1, 1),
